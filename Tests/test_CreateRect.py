7f2ce931d1e500f1ac7dfb94a3de1c9bb83e1028
phulikely <phulikely@gmail.com>
2022-06-08 15:35:01 +0700

    adding request_id

--- Tests/test_CreateRect.py
+++ Tests/test_CreateRect.py
@@ -1,10 +1,10 @@
-import time
-import pytest
+import json
 from Config.config import TestData
 from Tests.test_base import BaseTest
 from Pages.LoginPage import LoginPage
 from Pages.MainPage import MainPage
 from selenium.webdriver.common.by import By
+from seleniumwire.utils import decode
 
 
 class Test_Rect(BaseTest):
@@ -21,6 +21,16 @@ class Test_Rect(BaseTest):
         self.main_page = MainPage(self.driver)
         self.main_page.create_rect(length=555, width=666, height=777, processing_time=15)
         actual = self.main_page.get_result_text((By.XPATH, TestData.RESULT_TXT))        
+
+        for request in self.driver.requests:
+            if request.response and 'request_id' in request.url:
+                # print(request.url.split('?')[-1])
+                # break
+                response = request.response
+                body = decode(response.body, response.headers.get('Content-Encoding', 'identity'))
+                        # if request.url == "https://api.3dd-toolbox.com/ricoh-api/uploadfile":
+                print(json.loads(body))
+                break
 
         assert actual != 'No file result'
 
